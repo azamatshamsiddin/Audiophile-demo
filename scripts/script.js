a6cdcd5eb5d0cce7833c201df711fd99b9c18199
Azamat <muratovichazamat@yandex.com>
2021-06-05 20:01:16 +0500

    finished

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,4 @@
 console.log('hey!');
-// const burger_mneu = document.getElementById('burger_mneu');
-// const menu_container = document.getElementById('menu_container');
-
-// burger_mneu.addEventListener('click', () => {
-//     menu_container.classList.add('show');
-// });
-
-// let nav = false;
-
-// let burger_menu = function () {
-//   let getMenuConatiner = document.querySelector('.menu-container');
-//   let getMenu = document.querySelector('.menu');
-
-//   if (nav === false) {
-//     getMenuConatiner.style.visibility = 'visible';
-//     getMenu.style.height = '50%';
-
-//     let arrayLength = getMenu.length;
-//     for (let i=0; i< arrayLength; i++){
-//       getMenu[i].style.height = '100%';
-//     }
-//     nav = true;
-//   }
-
-// }
 
 function nav() {
   document.getElementById('menu_container').classList.toggle('show');
